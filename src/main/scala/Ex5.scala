bae5c01e0e49fb40adde2b3408895b1b6a8a6016
Daniel Franco López <Danifranco17lo@gmail.com>
2022-02-25 09:18:19 +0100

    Fixed Exercise 5
    Fixed issue with unix timestamp conversion.

--- src/main/scala/Ex5.scala
+++ src/main/scala/Ex5.scala
@@ -9,14 +9,19 @@ object Ex5 {
       .load("src/main/resources/retail_db/orders_parquet")
 
 
+
     val output = data.filter(col("order_status") === "COMPLETE")
       .select(col("order_id"),
-        to_date(from_unixtime(col("order_date") / 1000,"dd-MM-YYYY"),"dd-MM-YYYY").as("order_date"),
+        to_date(from_unixtime(col("order_date") / 1000,"yyyy-MM-dd HH:mm:ss"),"yyyy-MM-dd HH:mm:ss").as("order_date"),
         col("order_status"))
       .filter(year(col("order_date")) === lit(2014)&&
         ( month(col("order_date")) === lit(1)||
           month(col("order_date")) === lit(7) )
-    )
+      )
+      .select(col("order_id"),
+        date_format(col("order_date"),"dd-MM-yyyy").as("order_date"),
+        col("order_status"))
+
 
 
 
