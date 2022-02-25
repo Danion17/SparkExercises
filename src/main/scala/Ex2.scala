6402d03551d99639fb4d59cfdfc54cab8102ca35
Daniel Franco López <Danifranco17lo@gmail.com>
2022-02-25 09:40:58 +0100

    Fixing exercise 2
    Fixed sorting by column id, being id a string instead of an int

--- src/main/scala/Ex2.scala
+++ src/main/scala/Ex2.scala
@@ -8,7 +8,12 @@ object Ex2 {
     val data = sparkSession.read
       .csv("src/main/resources/retail_db/categories")
 
-    val output = data.orderBy(desc("_c0")).select(col("_c0"),col("_c2"))
+
+
+    val output = data.select(col("_c0").cast("INT").as("id")
+      ,col("_c2").as("name")).orderBy(desc("id"))
+
+
 
     output.write.format("csv").mode("overwrite")
       .option("header",true).option("sep",":")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -39,3 +39,5 @@ object Main extends App{
 
 }
 
+
+
